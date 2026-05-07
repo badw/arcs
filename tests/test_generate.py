04fedc511b7f0d84c68612f488d935478ebf035a
Benjamin Williamson <benjamin.williamson.10@ucl.ac.uk>
2026-05-07 11:31:26 +0200

    fixed tests

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,26 +1,33 @@
 from csv import Error
-import numpy as np 
-import json 
+import numpy as np
+import json
 from monty.serialization import loadfn
 from arcs.generate import GetEnergyandVibrationsVASP
 from arcs.generate import ReactionGibbsandEquilibrium
 from arcs.generate import GraphGenerator
-from arcs.generate import GenerateInitialConcentrations 
+from arcs.generate import GenerateInitialConcentrations
+from arcs.generate import parse_molecule
 from ase.io import read
 from ase.atoms import Atoms
 import pytest
 
 
-#GetEnergyandVibrationsVASP
+# GetEnergyandVibrationsVASP
+def test_parse_molecule():
+    assert parse_molecule("H2O") == {'H': 2, 'O': 1}
+    assert parse_molecule("(CH3_2CHCH2NH2") == {'C': 4, 'H': 11, 'N': 1} 
 
 def test_get_initial_magnetic_moments():
-    gevv = GetEnergyandVibrationsVASP(relax_directory='./vaspdata/relax/',vibrations_directory='./vaspdata/vibrations/')
+    gevv = GetEnergyandVibrationsVASP(
+        relax_directory='./vaspdata/relax/', vibrations_directory='./vaspdata/vibrations/')
     aseatoms = read('./vaspdata/relax/POSCAR')
     magmoms = gevv.get_initial_magnetic_moments(aseatoms)
-    assert magmoms == [0,0,0]
+    assert magmoms == [0, 0, 0]
+
 
 def test_get_atoms():
-    gevv = GetEnergyandVibrationsVASP(relax_directory='./vaspdata/relax/',vibrations_directory='./vaspdata/vibrations/')
+    gevv = GetEnergyandVibrationsVASP(
+        relax_directory='./vaspdata/relax/', vibrations_directory='./vaspdata/vibrations/')
     aseatoms = gevv.get_atoms()
 
     testaseatoms = Atoms.fromdict({'numbers': np.array([6, 8, 8]),
@@ -33,42 +40,55 @@ def test_get_atoms():
                                                      [0., 10.,  0.],
                                                      [0.,  0., 10.]]),
                                    'pbc': np.array([True,  True,  True])})
-    
-    assert list(aseatoms.todict()['numbers']) == list(testaseatoms.todict()['numbers'])
-    #can add more here
+
+    assert list(aseatoms.todict()['numbers']) == list(
+        testaseatoms.todict()['numbers'])
+    # can add more here
+
 
 def test_get_energy():
-    gevv = GetEnergyandVibrationsVASP(relax_directory='./vaspdata/relax/',vibrations_directory='./vaspdata/vibrations/')
+    gevv = GetEnergyandVibrationsVASP(
+        relax_directory='./vaspdata/relax/', vibrations_directory='./vaspdata/vibrations/')
     energy = gevv.get_energy()
 
     assert energy == -28.38822683
 
+
 def test_get_spin():
-    gevv = GetEnergyandVibrationsVASP(relax_directory='./vaspdata/relax/',vibrations_directory='./vaspdata/vibrations/')
+    gevv = GetEnergyandVibrationsVASP(
+        relax_directory='./vaspdata/relax/', vibrations_directory='./vaspdata/vibrations/')
     spin = gevv.get_spin()
 
     assert spin == 0
 
+
 def test_get_pointgroup():
-    gevv = GetEnergyandVibrationsVASP(relax_directory='./vaspdata/relax/',vibrations_directory='./vaspdata/vibrations/')
+    gevv = GetEnergyandVibrationsVASP(
+        relax_directory='./vaspdata/relax/', vibrations_directory='./vaspdata/vibrations/')
     pointgroup = gevv.get_pointgroup()
 
     assert pointgroup == 'D*h'
 
+
 def test_islinear():
-    gevv = GetEnergyandVibrationsVASP(relax_directory='./vaspdata/relax/',vibrations_directory='./vaspdata/vibrations/')
+    gevv = GetEnergyandVibrationsVASP(
+        relax_directory='./vaspdata/relax/', vibrations_directory='./vaspdata/vibrations/')
     linear = gevv.islinear()
 
     assert linear == 'linear'
 
+
 def test_get_rotation_num():
-    gevv = GetEnergyandVibrationsVASP(relax_directory='./vaspdata/relax/',vibrations_directory='./vaspdata/vibrations/')
+    gevv = GetEnergyandVibrationsVASP(
+        relax_directory='./vaspdata/relax/', vibrations_directory='./vaspdata/vibrations/')
     rotation_num = gevv.get_rotation_num()
 
     assert rotation_num == 2
 
+
 def test_get_vibrations():
-    gevv = GetEnergyandVibrationsVASP(relax_directory='./vaspdata/relax/',vibrations_directory='./vaspdata/vibrations/')
+    gevv = GetEnergyandVibrationsVASP(
+        relax_directory='./vaspdata/relax/', vibrations_directory='./vaspdata/vibrations/')
     vibrations = gevv.get_vibrations()
 
     assert vibrations == [0.304843445,
@@ -80,51 +100,62 @@ def test_get_vibrations():
                           -7.8669e-05,
                           -0.000301184,
                           -0.001327542]
-    
+
+
 def test_as_dict():
-    gevv = GetEnergyandVibrationsVASP(relax_directory='./vaspdata/relax/',vibrations_directory='./vaspdata/vibrations/')
+    gevv = GetEnergyandVibrationsVASP(
+        relax_directory='./vaspdata/relax/', vibrations_directory='./vaspdata/vibrations/')
     _dict = gevv.as_dict()
 
-    #add an assert
+    # add an assert
 
-#ReactionGibbsandEquilibrium
+# ReactionGibbsandEquilibrium
+
+
 def test_json_data_loader():
-    json.load(open('test_dft_data.json'))
+    json.load(open('test_quantum_data.json'))
+
 
 def test_Gibbs():
-    dft_data = loadfn('test_dft_data.json')
-    rge = ReactionGibbsandEquilibrium(reaction_input=dft_data)
-    gibbs_free_energy = rge.Gibbs(dft_dict=dft_data['CO2'],temperature=100,pressure=1)
-    assert gibbs_free_energy == -31.974842281859573
+    quantum_data = loadfn('test_quantum_data.json')
+    rge = ReactionGibbsandEquilibrium(reaction_input=quantum_data)
+    gibbs_free_energy = rge.Gibbs(
+        quantum_dict=quantum_data['CO2'], temperature=100, pressure=1)
+    assert gibbs_free_energy == pytest.approx(-5119.272188209697)
+
 
 def test_reaction_gibbs():
-    dft_data = loadfn('test_dft_data.json')
-    rge = ReactionGibbsandEquilibrium(reaction_input=dft_data)
-    reaction = dft_data['reactions'][str(307)]
+    quantum_data = loadfn('test_quantum_data.json')
+    rge = ReactionGibbsandEquilibrium(reaction_input=quantum_data)
+    reaction = quantum_data['reactions'][str(307)]
     reaction_gibbs = rge.reaction_gibbs(
         reaction=reaction,
         pressure=1,
         temperature=100
     )
-    assert reaction_gibbs == pytest.approx(0.1236897822581895)
+    assert reaction_gibbs == pytest.approx(0.41032333892781025)
+
 
 def test_equilibrium_constant():
-    dft_data = loadfn('test_dft_data.json')
-    rge = ReactionGibbsandEquilibrium(reaction_input=dft_data)
+    quantum_data = loadfn('test_quantum_data.json')
+    rge = ReactionGibbsandEquilibrium(reaction_input=quantum_data)
     equilibrium_constant = rge.equilibrium_constant(gibbs_free_energy=-0.6184489112909475,
                                                     temperature=100)
     assert equilibrium_constant == 1.4738501117040624e+31
 
+
 def test_get_reaction_gibbs_and_equilibrium():
-    dft_data = loadfn('test_dft_data.json')
-    reaction = dft_data['reactions'][str(307)]
-    rge = ReactionGibbsandEquilibrium(reaction_input=dft_data)
-    _dict = rge.get_reaction_gibbs_and_equilibrium(reaction=reaction,temperature=100,pressure=1)
+    quantum_data = loadfn('test_quantum_data.json')
+    reaction = quantum_data['reactions'][str(307)]
+    rge = ReactionGibbsandEquilibrium(reaction_input=quantum_data)
+    _dict = rge.get_reaction_gibbs_and_equilibrium(
+        reaction=reaction, temperature=100, pressure=1)
 
-    assert _dict == {'g': pytest.approx(0.1236897822581895), 'k': pytest.approx(5.838608245434512e-07)}
+    assert _dict == {'g': pytest.approx(
+        0.41032333892781025), 'k': pytest.approx(2.0922514397133124e-21)}
 
 
-# test GraphGenerator
+#  test GraphGenerator
 
 def test_cost_function():
     applied_reactions = [{'g': 0.6184489112909475,
@@ -139,8 +170,8 @@ def test_cost_function():
         temperature=100,
         reactants=applied_reactions[0]['r']['reactants']
     )
-    
-    assert cost == pytest.approx(1.8028655802225437)
+
+    assert cost == pytest.approx(1.4084092098796381)
 
 
 def test_generate_multidigraph():
@@ -169,41 +200,42 @@ def test_generate_multidigraph():
                           'r': {'reaction_string': '1 O2 + 1 CH3COOH = 2 CH2O2',
                                 'reactants': {'O2': 1, 'CH3COOH': 1},
                                 'products': {'CH2O2': 2}}}]
-    
-    gg = GraphGenerator()
-    graph = gg.generate_multidigraph(temperature=100,applied_reactions=applied_reactions)
 
-    adjacency_map = {'H2O': {0: {0: {'weight': 5.7552529146395965}}},
-                      0: {'H2O': {0: {'weight': 0.0233957881009098}},
-                          'O2': {0: {'weight': 5.7552529146395965}},
-                          'H2': {0: {'weight': 5.7552529146395965}}},
-                      'O2': {0: {0: {'weight': 0.0233957881009098}},
-                             3: {0: {'weight': 0.5594504962328015}},
-                             4: {0: {'weight': 0.11464403583982867}}},
-                      'H2': {0: {0: {'weight': 0.0233957881009098}}},
-                      'CO2': {1: {0: {'weight': 1.4851013816575676}}},
-                      1: {'CO2': {0: {'weight': 1.1575530621227441}},
-                          'CH4': {0: {'weight': 1.1575530621227441}},
-                          'CH3COOH': {0: {'weight': 1.4851013816575676}}},
-                      'CH4': {1: {0: {'weight': 1.4851013816575676}}},
-                      'CH3COOH': {1: {0: {'weight':  1.1575530621227441}},
-                                  4: {0: {'weight': 0.11464403583982867}}},
-                      'CO': {2: {0: {'weight': 1.9474126879158}}},
-                      2: {'CO': {0: {'weight': 0.8064645879463394}},
-                          'CH3OH': {0: {'weight': 0.8064645879463394}},
-                          'H2CO': {0: {'weight': 1.9474126879158}}},
-                      'CH3OH': {2: {0: {'weight': 1.9474126879158}}},
-                      'H2CO': {2: {0: {'weight': 0.8064645879463394}}},
-                      3: {'O2': {0: {'weight':  2.392520284979471}},
-                          'HNO2': {0: {'weight': 2.392520284979471}},
-                          'HNO3': {0: {'weight': 0.5594504962328015}}},
-                      'HNO2': {3: {0: {'weight': 0.5594504962328015}}},
-                      'HNO3': {3: {0: {'weight': 2.392520284979471}}},
-                      4: {'O2': {0: {'weight': 4.132824427134435}},
-                          'CH3COOH': {0: {'weight': 4.132824427134435}},
-                          'CH2O2': {0: {'weight': 0.11464403583982867}}},
-                      'CH2O2': {4: {0: {'weight': 4.132824427134435}}}}
-    
+    gg = GraphGenerator()
+    graph = gg.generate_multidigraph(
+        temperature=100, applied_reactions=applied_reactions)
+
+    adjacency_map = {'H2O': {0: {0: {'weight': np.float64(1.9492014814446184)}}},
+                     0: {'H2O': {0: {'weight': np.float64(0.8053103344762139)}},
+                         'O2': {0: {'weight': np.float64(1.9492014814446184)}},
+                         'H2': {0: {'weight': np.float64(1.9492014814446184)}}},
+                     'O2': {0: {0: {'weight': np.float64(0.8053103344762139)}},
+                            3: {0: {'weight': np.float64(1.2234902236516356)}},
+                            4: {0: {'weight': np.float64(1.0985582213409335)}}},
+                     'H2': {0: {0: {'weight': np.float64(0.8053103344762139)}}},
+                     'CO2': {1: {0: {'weight': np.float64(1.3369778231103757)}}},
+                     1: {'CO2': {0: {'weight': np.float64(1.2959924751414802)}},
+                         'CH4': {0: {'weight': np.float64(1.2959924751414802)}},
+                         'CH3COOH': {0: {'weight': np.float64(1.3369778231103757)}}},
+                     'CH4': {1: {0: {'weight': np.float64(1.3369778231103757)}}},
+                     'CH3COOH': {1: {0: {'weight': np.float64(1.2959924751414802)}},
+                                 4: {0: {'weight': np.float64(1.0985582213409335)}}},
+                     'CO': {2: {0: {'weight': np.float64(1.389551610603596)}}},
+                     2: {'CO': {0: {'weight': np.float64(1.2451742189951533)}},
+                         'CH3OH': {0: {'weight': np.float64(1.2451742189951533)}},
+                         'H2CO': {0: {'weight': np.float64(1.389551610603596)}}},
+                     'CH3OH': {2: {0: {'weight': np.float64(1.389551610603596)}}},
+                     'H2CO': {2: {0: {'weight': np.float64(1.2451742189951533)}}},
+                     3: {'O2': {0: {'weight': np.float64(1.4126027501246856)}},
+                         'HNO2': {0: {'weight': np.float64(1.4126027501246856)}},
+                         'HNO3': {0: {'weight': np.float64(1.2234902236516356)}}},
+                     'HNO2': {3: {0: {'weight': np.float64(1.2234902236516356)}}},
+                     'HNO3': {3: {0: {'weight': np.float64(1.4126027501246856)}}},
+                     4: {'O2': {0: {'weight': np.float64(1.5532383356136172)}},
+                         'CH3COOH': {0: {'weight': np.float64(1.5532383356136172)}},
+                         'CH2O2': {0: {'weight': np.float64(1.0985582213409335)}}},
+                     'CH2O2': {4: {0: {'weight': np.float64(1.5532383356136172)}}}}
+
     nodes = ['H2O',
              0,
              'O2',
@@ -221,22 +253,21 @@ def test_generate_multidigraph():
              'HNO3',
              4,
              'CH2O2']
-    
+
     assert dict(graph.adjacency()) == adjacency_map
 
     assert list(graph.nodes()) == nodes
 
-
     def test_from_file():
 
-        return(None)
-    
+        return (None)
+
     def test_from_dict():
 
-        return(None)
+        return (None)
 
 
-#GenerateInitialConcentrations
+# GenerateInitialConcentrations
 
 def test_all_random():
     applied_reactions = [{'g': 4.747780176970167,
@@ -264,16 +295,18 @@ def test_all_random():
                           'r': {'reaction_string': '1 O2 + 1 CH3COOH = 2 CH2O2',
                                 'reactants': {'O2': 1, 'CH3COOH': 1},
                                 'products': {'CH2O2': 2}}}]
-    
+
     gg = GraphGenerator()
-    graph = gg.generate_multidigraph(temperature=100,applied_reactions=applied_reactions)
+    graph = gg.generate_multidigraph(
+        temperature=100, applied_reactions=applied_reactions)
     gic = GenerateInitialConcentrations(graph=graph)
-    
+
     try:
         gic.all_random()
     except Exception:
-        raise(Error)
-    
+        raise (Error)
+
+
 def test_all_zero():
     applied_reactions = [{'g': 4.747780176970167,
                           'k': 5.277282624067824e-240,
@@ -300,9 +333,10 @@ def test_all_zero():
                           'r': {'reaction_string': '1 O2 + 1 CH3COOH = 2 CH2O2',
                                 'reactants': {'O2': 1, 'CH3COOH': 1},
                                 'products': {'CH2O2': 2}}}]
-    
+
     gg = GraphGenerator()
-    graph = gg.generate_multidigraph(temperature=100,applied_reactions=applied_reactions)
+    graph = gg.generate_multidigraph(
+        temperature=100, applied_reactions=applied_reactions)
     gic = GenerateInitialConcentrations(graph=graph)
     all_zero = gic.all_zero()
 
@@ -318,7 +352,8 @@ def test_all_zero():
                         'HNO2': 0,
                         'HNO3': 0,
                         'CH2O2': 0}
-    
+
+
 def test_specific_random():
     applied_reactions = [{'g': 4.747780176970167,
                           'k': 5.277282624067824e-240,
@@ -345,15 +380,17 @@ def test_specific_random():
                           'r': {'reaction_string': '1 O2 + 1 CH3COOH = 2 CH2O2',
                                 'reactants': {'O2': 1, 'CH3COOH': 1},
                                 'products': {'CH2O2': 2}}}]
-    
+
     gg = GraphGenerator()
-    graph = gg.generate_multidigraph(temperature=100,applied_reactions=applied_reactions)
+    graph = gg.generate_multidigraph(
+        temperature=100, applied_reactions=applied_reactions)
     gic = GenerateInitialConcentrations(graph=graph)
-    specific_random = gic.specific_random(compounds=['H2O','CO'])
+    specific_random = gic.specific_random(compounds=['H2O', 'CO'])
 
     assert specific_random['H2O'] > 0
     assert specific_random['CO'] > 0
     assert specific_random['O2'] == 0
+
 
 def test_update_ic():
     applied_reactions = [{'g': 4.747780176970167,
@@ -381,24 +418,13 @@ def test_update_ic():
                           'r': {'reaction_string': '1 O2 + 1 CH3COOH = 2 CH2O2',
                                 'reactants': {'O2': 1, 'CH3COOH': 1},
                                 'products': {'CH2O2': 2}}}]
-    
+
     gg = GraphGenerator()
-    graph = gg.generate_multidigraph(temperature=100,applied_reactions=applied_reactions)
+    graph = gg.generate_multidigraph(
+        temperature=100, applied_reactions=applied_reactions)
     gic = GenerateInitialConcentrations(graph=graph)
-    specific_random = gic.update_ic(update_dict={'CO2':1,'H2O':3})
+    specific_random = gic.update_ic(update_dict={'CO2': 1, 'H2O': 3})
 
     assert specific_random['H2O'] == 3
     assert specific_random['CO2'] == 1
     assert specific_random['O2'] == 0
-
-
-
-
-
-
-
-
-
-
-    
-
